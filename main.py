f7cc45dc59d4e6e3c69f469c9f8f48256bec5496
unknown <thejas_b@yahoo.co.in>
2024-11-17 17:57:03 +0530

    Changes in the UI

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ from flask import Flask, redirect, url_for, render_template
 app=Flask(__name__,template_folder="templates")
 @app.route("/")
 def base():
-    return render_template("/templates/home.html")
+    return render_template("home.html")
 
-@app.route("/home")
+@app.route("/home.html")
 def home():
     return render_template("home.html")
 
-@app.route("/explore")
+@app.route("/explore.html")
 def explore():
     return render_template("explore.html")
 
-@app.route("/info")
+@app.route("/info.html")
 def info():
     return render_template("info.html")
 
-@app.route("/filter")
+@app.route("/filter.html")
 def filter():
     return render_template("filter.html")
 
-@app.route("/save")
+@app.route("/save.html")
 def save():
     return render_template("save.html")
 if __name__=="__main__":
